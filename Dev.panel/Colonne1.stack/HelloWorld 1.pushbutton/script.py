57c1cb4b95f4743243d4ac26bd381453abbce09c
Antoinelvt <130642291+Antoinelvt@users.noreply.github.com>
2023-04-14 17:14:31 +0200

    Add files via upload

---
+++ Dev.panel/Colonne1.stack/HelloWorld 1.pushbutton/script.py
@@ -0,0 +1,14 @@
+__title__ = "Informations generales 1"
+__author__ = "Antoine Levert"
+__doc__ = """Ceci est le bouton d'informations"""
+
+
+#VARIABLES
+uidoc = __revit__.ActiveUIDocument
+
+# CUSTOM IMPORT
+from Snippets._selection import get_selected_elements
+
+if __name__ == '__main__':
+    print (get_selected_elements(uidoc))
+
